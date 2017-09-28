00b4c0bc7b8b5c2c569e0582e0d43dd448934d50
CJRocK <chetanjoshi1995@gmail.com>
2017-09-28 10:22:59 +0000

    Add files via upload

---
+++ deploy.ps1
@@ -0,0 +1,58 @@
+﻿Param
+(
+[Parameter(Mandatory=$true)][String]$ComputerName,
+[Parameter(Mandatory=$true)][String]$MembersId,
+[Parameter(Mandatory=$true)][System.Management.Automation.PSCredential]$Credentials,
+[Parameter(Mandatory=$true)][String]$DomainNetbiosName    
+)
+
+$ConfigData = @{
+    AllNodes = @(
+        @{
+            NodeName=$ComputerName;
+            PSDscAllowPlainTextPassword = $true
+            
+         }
+)}
+
+configuration AllowRemoteDesktopAdminConnections
+{
+
+
+[System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Credentials.UserName)", $Credentials.Password)
+[String]$DomainMember=$DomainNetbiosName+"\"+$MembersId
+    node ($ComputerName)
+    {        
+        Group ConfigureRDPGroup
+        {
+           Ensure = 'Present'
+           GroupName = "Remote Desktop Users"
+           Members =  @($DomainMember, "wtw_test\chetan123adm" ) 
+           Credential = $DomainCreds
+        }
+    
+
+        Group GroupAdmins
+         {
+	        GroupName = "GroupAdmins"
+        	Ensure = "Present"
+	        Members = @($DomainMember, "wtw_test\chetan123adm" )
+            Credential = $DomainCreds
+         }
+
+        Group Administrators
+         {
+        	GroupName = "Administrators"
+	        Ensure = "Present"
+	        MembersToInclude =  "GroupAdmins"
+	        DependsOn = "[Group]GroupAdmins"
+            Credential = $DomainCreds
+         }
+        
+    }
+}
+$workingdir = 'C:\DSC\MOF'
+AllowRemoteDesktopAdminConnections -ConfigurationData $ConfigData -OutputPath $workingdir
+
+Start-DscConfiguration -ComputerName 'N21-BN-DVTS082D' -wait -force -verbose -path $workingdir 
+
